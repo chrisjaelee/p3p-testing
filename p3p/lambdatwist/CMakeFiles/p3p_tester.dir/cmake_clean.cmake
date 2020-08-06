20e5c5491cad6569bdf50ce3bc4e8b541fdee562
chrisjaelee <clee@carnegierobotics.com>
2020-08-06 14:04:39 -0400

    mostly working code, but segmentation fault is given if ran for more than 58 iterations

---
+++ p3p/lambdatwist/CMakeFiles/p3p_tester.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p3p_tester.dir/main.cpp.o"
+  "p3p_tester"
+  "p3p_tester.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p3p_tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
